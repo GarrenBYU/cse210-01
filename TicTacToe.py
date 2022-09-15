1d023912c64545ba87c20c0c0d6a7c4cdf399d29
GarrenBYU <mei21004@byui.edu>
2022-09-14 22:33:32 -0600

    added the in between lines

--- TicTacToe.py
+++ TicTacToe.py
@@ -3,11 +3,14 @@
 def main():
     grid()
 def grid():
-    lineOne = ["1 ", "| ", "2 ", "| ", "3 "]
-    lineTwo = ["4 ", "| ", "5 ", "| ", "6 "]
-    lineThree = ["7 ", "| ", "8 ", "| ", "9 "]
-    print(lineOne)
-    print(lineTwo)
-    print(lineThree)
+    lineOne = ["1", "|", "2", "|", "3"]
+    lineTwo = ["4", "|", "5", "|", "6"]
+    lineThree = ["7", "|", "8", "|", "9"]
+    inbetweenLine = ["-", "+", "-", "+", "-"]
+    print(*lineOne)
+    print(*inbetweenLine)
+    print(*lineTwo)
+    print(*inbetweenLine)
+    print(*lineThree)
 
 main()
