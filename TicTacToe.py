5c5f8ec81a04798468be441ce85474f70db83be0
GarrenBYU <mei21004@byui.edu>
2022-09-14 23:44:35 -0600

    move function was made

--- TicTacToe.py
+++ TicTacToe.py
@@ -6,6 +6,7 @@ def main():
     lineThree = []
     inbetweenLine=[]
     grid()
+
 def grid():
     lineOne = ["1", "|", "2", "|", "3"]
     lineTwo = ["4", "|", "5", "|", "6"]
@@ -17,5 +18,19 @@ def grid():
     print(*inbetweenLine)
     print(*lineThree)
     return lineOne and lineTwo and lineThree and inbetweenLine
+
+def move(line, place, number, person):
+    if (person == 1):
+        if(line[place] != "X" or line[place] != "O"):
+            line = line.replace(number, "X")
+            return line
+        else:
+            print("That spot has already been taken")
+    else:
+        if(line[place] != "X" or line[place] != "O"):
+            line = line.replace(number, "O")
+            return line
+        else:
+            print("That spot has already been taken")
 
 main()
