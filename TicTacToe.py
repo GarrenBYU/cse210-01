12581b56f70b6899d2beb091f77624ec087a4ff3
GarrenBYU <mei21004@byui.edu>
2022-09-19 20:49:58 -0600

    Finally got the code for if the spot taken

--- TicTacToe.py
+++ TicTacToe.py
@@ -8,10 +8,13 @@ OGlineOne = ["1", "|", "2", "|", "3"]
 OGlineTwo = ["4", "|", "5", "|", "6"]
 OGlineThree = ["7", "|", "8", "|", "9"]
 inbetweenLine = ["-", "+", "-", "+", "-"]
+usedNumbers = []
 def main():
     player = 1
     i = 0
     win=0
+    desicionTwo = '10'
+    n= 0
     #grid
     lineOne = OGlineOne
     lineTwo = OGlineTwo
@@ -22,7 +25,12 @@ def main():
     print(*inbetweenLine)
     print(*lineThree)
     while(win == 0):
-        desicionOne = input("What number do you want to place Player 1? ")
+        while(n == 0 or 0 >= int(desicionOne) > 9 or w == True):
+            desicionOne = input("What number do you want to place Player 1? ")
+            n=1
+            w = usedNumber(desicionOne, lineOne, lineTwo, lineThree, OGGridOne, OGGridTwo, OGGridThree)
+        n=0
+        w=True
         move(desicionOne, 1, lineOne, lineTwo, lineThree)
         print(*lineOne)
         print(*inbetweenLine)
@@ -34,7 +42,12 @@ def main():
         i = i+1
         if(i >= 9):
             break
-        desicionTwo = input("What number do you want to place Player 2? ")
+        while(n == 0 or 0 >= int(desicionTwo) > 9 or w == True):
+            desicionTwo = input("What number do you want to place Player 2? ")
+            n=1
+            w = usedNumber(desicionTwo, lineOne, lineTwo, lineThree, OGGridOne, OGGridTwo, OGGridThree)
+        n = 0
+        w=True
         move(desicionTwo, 2, lineOne, lineTwo, lineThree)
         if(victory(desicionOne, lineOne, lineTwo, lineThree, win) == 1):
             break
@@ -108,6 +121,32 @@ def victory(number, line1, line2, line3, score):
     elif (line1[4] == line2[2] == line3[0]):
         score = 1
         return score
+
+def usedNumber(number, line1, line2, line3, OG1, OG2, OG3):
+    if (0 < int(number) < 4 ):
+        place = OG1.index(number)
+        if(OG1[place] == line1[place]):
+            return False
+        else:
+            print("That spot was already taken")
+            return True
+    elif (3 < int(number) < 7):
+        place = OG2.index(number)
+        if(OG2[place] == line2[place]):
+            return False
+        else:
+            print("That spot was already taken")
+            return True
+    elif (6 < int(number) < 10):
+        place = OG3.index(number)
+        if(OG3[place] == line3[place]):
+            return False
+        else:
+            print("That spot was already taken")
+            return True
+    else:
+        print("This number or place is not an option")
+        return True
 
 if __name__ == "__main__":
     main()
