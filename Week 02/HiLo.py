7dea18b35a89915aacae3f3faa176c8af1e6e3d4
GarrenBYU <mei21004@byui.edu>
2022-09-23 22:16:51 -0600

    finished the again function

--- Week 02/HiLo.py
+++ Week 02/HiLo.py
@@ -5,7 +5,6 @@ class Player:
     points = 300
     guess = ''
     guessint = NULL
-    again = ""
     def guessNumber(self):
         guessint = int(self.guess)
         return guessint
@@ -17,17 +16,21 @@ class Card:
         return self.value
 
 def main():
-    card = Card
-    card.value = (random.randint(1, 13))
-    OGcard = card.value
-    print(f"The card is: {card.value} ")
-    next_card = Card
-    next_card.value = (random.randint(1, 13))
-    Player.guess = input("Higher or Lower? [h/l] ")
-    hiLo(OGcard, next_card.value)
-    print(f"Next card was: {next_card.value}")
-    score = Player.points
-    print(f"Your score is: {score}")
+    again = 1
+    while(again == 1):
+        card = Card
+        card.value = (random.randint(1, 13))
+        OGcard = card.value
+        print(f"The card is: {card.value} ")
+        next_card = Card
+        next_card.value = (random.randint(1, 13))
+        Player.guess = input("Higher or Lower? [h/l] ")
+        hiLo(OGcard, next_card.value)
+        print(f"Next card was: {next_card.value}")
+        score = Player.points
+        print(f"Your score is: {score}")
+        again = input("Play again? [y/n] ")
+        playAgain(again)
 
 def hiLo(current, next):
     if(Player.guess == "l"):
@@ -44,6 +47,13 @@ def hiLo(current, next):
         else:
             Player.points = Player.points - 75
             return Player.points
+
+def playAgain(decision):
+    if(decision == 'y'):
+        addiction = 1
+    else:
+        addiction = 2
+        print(f"Congrats you dont have a gambling addiction. 😉 Your end score was {Player.score}")
 
 if __name__ == "__main__":
     main()
