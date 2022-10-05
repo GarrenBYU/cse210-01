06959d844800588638ccc18c5aa2747275d7665d
GarrenBYU <mei21004@byui.edu>
2022-10-05 02:21:46 -0600

    finished. This is an interesting game

--- rfk-incomplete/rfk/game/casting/artifact.py
+++ rfk-incomplete/rfk/game/casting/artifact.py
@@ -3,5 +3,11 @@ from game.casting.actor import Actor
 
 # TODO: Implement the Artifact class here. Don't forget to inherit from Actor!
 class Artifact(Actor):
+    
+    def __init__(self):
+        self._message = ""
+
     def set_message(self, message):
-        return message
+        self._message = message
+    def get_message(self):
+        return self._message
